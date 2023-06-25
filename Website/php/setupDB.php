d6077a941a3a0a7ceea67c0ebaad1e59fe840823
claras000 <clara.o.correa@web.de>
2023-06-25 11:32:38 +0200

    datenbankabfrage test

--- Website/php/setupDB.php
+++ Website/php/setupDB.php
@@ -1,15 +1,35 @@
-<?
-try {
-  $dsn = 'mysql:host=localhost;dbname=omemory;charset=utf8mb4';
-  $username = 'localhost';
-  $password = '';
-  $dbh = new \PDO($dsn, $username, $password);
-} catch(\Exception $e) {
-  die('Interner Fehler: Die Datenbank-Verbindung konnte nicht aufgebaut werden.');
+
+<?php
+$servername = "localhost"; // Verbindung zum Server
+$username = "root"; // Benutzername für den Datenbankzugriff (Standard: "root")
+$password = ""; // Passwort für den Datenbankzugriff (Standard: "")
+$dbname = "omemory"; // Name deiner Datenbank
+
+// Verbindung zur Datenbank herstellen
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Überprüfen, ob die Verbindung erfolgreich hergestellt wurde
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
 }
 
 
 
+// Beispielabfrage
+$sql = "SELECT spieler FROM deineTabelle";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Daten ausgeben
+    while($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"]. " - spielName: " . $row["spielname"]. "<br>";
+    }
+} else {
+    echo "Keine Ergebnisse gefunden.";
+}
+
+// Verbindung schließen
+$conn->close();
 ?>
 
 
