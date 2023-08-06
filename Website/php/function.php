b6b545987cb608ddf6f34194e38688b3a8d1e76e
Kris149x <131782916+Kris149x@users.noreply.github.com>
2023-08-06 16:26:52 +0200

    ADD: Register and Login

---
+++ Website/php/function.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost", "root", "", "omemory");
+
+// IF
+if(isset($_POST["action"])){
+  if($_POST["action"] == "register"){
+    register();
+  }
+  else if($_POST["action"] == "login"){
+    login();
+  }
+}
+
+
+// REGISTER
+function register(){
+  global $conn;
+
+  $vorname = $_POST["vorname"];
+  $nachname = $_POST["nachname"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+
+  if(empty($vorname) || empty($nachname) || empty($email) || empty($password)){
+    echo "Please Fill Out The Form!";
+    exit;
+  }
+
+  $user = mysqli_query($conn, "SELECT * FROM spieler WHERE email = '$email'");
+  if(mysqli_num_rows($user) > 0){
+    echo "email is Already Taken";
+    exit;
+  }
+
+  $query = "INSERT INTO spieler(vorname, nachname, email, password) VALUES('$vorname', '$nachname', '$email', '$password')";
+  mysqli_query($conn, $query);
+  echo "Registration Successful";
+  exit;
+}
+
+
+// LOGIN
+function login(){
+  global $conn;
+
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+
+  $user = mysqli_query($conn, "SELECT * FROM spieler WHERE email = '$email'");
+
+  if(mysqli_num_rows($user) > 0){
+
+    $row = mysqli_fetch_assoc($user);
+
+    if($password == $row['password']){
+      echo "Login Successful";
+      $_SESSION["login"] = true;
+      $_SESSION["id"] = $row["id"];
+    }
+    else{
+      echo "Wrong Password";
+      exit;
+    }
+  }
+  else{
+    echo "User Not Registered";
+    exit;
+  }
+}
+?>
