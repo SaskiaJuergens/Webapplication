15101f467a5132bd615ef3515116b8891ba026af
claras000 <clara.o.correa@web.de>
2023-08-08 21:04:07 +0200

    spielerseite

--- Website/JS/SpielerDetail.js
+++ Website/JS/SpielerDetail.js
@@ -1,208 +1,8 @@
 var playerName; //konstante für Hover und weiterleiten auf Playerseite
+var spielerList = [];
 
-var playerData = {
-  Habul: [
-    {
-      date: "01.06.2022",
-      level: "1",
-      duration: "10:00",
-      playerCount: "1",
-      winnerName: "Mustermann",
-      status: "abgebrochen",
-    },
-    {
-      date: "16.05.2006",
-      level: "3",
-      duration: "0",
-      playerCount: "1",
-      winnerName: "-",
-      status: "gewonnen",
-    },
-    {
-      date: "02.04.2000",
-      level: "2",
-      duration: "6:00",
-      playerCount: "1",
-      winnerName: "-",
-      status: "gewonnen",
-    },
-    {
-      date: "01.06.2022",
-      level: "1",
-      duration: "15:00",
-      playerCount: "2",
-      winnerName: "Habul",
-      status: "verloren",
-    },
-    {
-      date: "24.02.2023",
-      level: "2",
-      duration: "09:00",
-      playerCount: "2",
-      winnerName: "Müller",
-      status: "abgelaufen",
-    },
-    {
-      date: "12.08.2022",
-      level: "2",
-      duration: "10:00",
-      playerCount: "2",
-      winnerName: "Mustermann",
-      status: "gewonnen",
-    },
-    {
-      date: "01.07.2022",
-      level: "3",
-      duration: "16:00",
-      playerCount: "1",
-      winnerName: "-",
-      status: "gewonnen",
-    },
-    {
-      date: "30.04.2023",
-      level: "2",
-      duration: "02:00",
-      playerCount: "1",
-      winnerName: "-",
-      status: "gewonnen",
-    },
-    // Weitere Spielverlaufsinformationen hier hinzufügen
-  ],
-  Mustermann: [
-    {
-      date: "01.06.2052",
-      level: "144",
-      duration: "10:00",
-      playerCount: "1",
-      winnerName: "Mustermann",
-      status: "beendet",
-    },
-    {
-      date: "16.05.2046",
-      level: "33",
-      duration: "0",
-      playerCount: "1",
-      winnerName: "-",
-      status: "beendet",
-    },
-    {
-      date: "02.04.2030",
-      level: "16",
-      duration: "6:00",
-      playerCount: "1",
-      winnerName: "-",
-      status: "beendet",
-    },
-    {
-      date: "01.06.2022",
-      level: "1",
-      duration: "15:00",
-      playerCount: "2",
-      winnerName: "Habul",
-      status: "beendet",
-    },
-    {
-      date: "09.01.2023",
-      level: "2",
-      duration: "09:00",
-      playerCount: "2",
-      winnerName: "Müller",
-      status: "beendet",
-    },
-    {
-      date: "10.03.2022",
-      level: "2",
-      duration: "10:00",
-      playerCount: "2",
-      winnerName: "Mustermann",
-      status: "beendet",
-    },
-    {
-      date: "01.07.2022",
-      level: "3",
-      duration: "16:00",
-      playerCount: "1",
-      winnerName: "-",
-      status: "nicht beendet",
-    },
-    {
-      date: "04.09.2021",
-      level: "2",
-      duration: "02:00",
-      playerCount: "1",
-      winnerName: "-",
-      status: "nicht beendet",
-    },
-  ],
-  Müller: [
-    {
-      date: "04.06.2012",
-      level: "2",
-      duration: "10:00",
-      playerCount: "1",
-      winnerName: "Mustermann",
-      status: "beendet",
-    },
-    {
-      date: "16.05.2000",
-      level: "3",
-      duration: "0",
-      playerCount: "1",
-      winnerName: "-",
-      status: "beendet",
-    },
-    {
-      date: "02.04.1999",
-      level: "1",
-      duration: "6:00",
-      playerCount: "1",
-      winnerName: "-",
-      status: "beendet",
-    },
-    {
-      date: "01.06.2022",
-      level: "1",
-      duration: "15:00",
-      playerCount: "2",
-      winnerName: "Habul",
-      status: "beendet",
-    },
-    {
-      date: "24.02.2023",
-      level: "2",
-      duration: "09:00",
-      playerCount: "2",
-      winnerName: "Müller",
-      status: "beendet",
-    },
-    {
-      date: "24.08.2018",
-      level: "2",
-      duration: "10:00",
-      playerCount: "2",
-      winnerName: "Mustermann",
-      status: "beendet",
-    },
-    {
-      date: "01.01.2022",
-      level: "1",
-      duration: "16:00",
-      playerCount: "1",
-      winnerName: "-",
-      status: "nicht beendet",
-    },
-    {
-      date: "01.04.2023",
-      level: "1",
-      duration: "02:00",
-      playerCount: "1",
-      winnerName: "-",
-      status: "nicht beendet",
-    },
-  ],
-};
-
-playerData = []; //Eventhandler für HTML
+var playerData = []; //Eventhandler für HTML
+var currentID;
 
 window.addEventListener("load", setup);
 function setup() {
@@ -212,7 +12,9 @@ function setup() {
   const urlParams = new URLSearchParams(queryString);
   const product = urlParams.get("elem");
   console.log(product);
+  showSpieler();
   showSpiel();
+
   // showPlayerPage(product);
 }
 
@@ -317,11 +119,51 @@ function ajaxShowSpiel(event) {
       initiator: initiator,
     };
 
+    // Tabelle-Rumpf
+    var tbody = document.getElementById("resultCard");
+    for (var i = 0; i < myObj.length; i++) {
+      var tr = document.createElement("tr");
+
+      var td1 = document.createElement("td");
+      var einzeln = myObj[i]["einzeln"];
+      td1.appendChild(document.createTextNode(einzeln));
+      tr.appendChild(td1);
+
+      var td1 = document.createElement("td");
+      var Datetime = myObj[i]["Datetime"];
+      td1.appendChild(document.createTextNode(Datetime));
+      tr.appendChild(td1);
+
+      var td1 = document.createElement("td");
+      var verlauf = myObj[i]["verlauf"];
+      td1.appendChild(document.createTextNode(verlauf));
+      tr.appendChild(td1);
+
+      var td1 = document.createElement("td");
+      var mitspieler = getPlayerByID(spielerList, myObj[i]["mitspieler"]);
+      td1.appendChild(document.createTextNode(initiator.mitspieler));
+      tr.appendChild(td1);
+
+      var td1 = document.createElement("td");
+      var gewinner = getPlayerByID(spielerList, myObj[i]["gewinner"]);
+      td1.appendChild(document.createTextNode(gewinner.spielname));
+      tr.appendChild(td1);
+
+      var td1 = document.createElement("td");
+      var initiator = getPlayerByID(spielerList, myObj[i]["initiator"]);
+      td1.appendChild(document.createTextNode(initiator.spielname));
+      tr.appendChild(td1);
+
+      tbody.appendChild(tr);
+    }
+
     console.log(item);
     // Das erstellte Objekt der Liste hinzuf�gen
     playerData.push(item);
   }
   console.log(playerData);
+  var tupel = getPlayerByID(spielerList, 3);
+  console.log("tupel: " + tupel.spielname);
 }
 
 // Falls eine Ajax-Anfrage gescheitert ist ...
@@ -329,8 +171,60 @@ function ajaxFehler(event) {
   alert(event.target.statusText);
 }
 
-function displayPlayerGameData(playerID) {
-  var table = document.getElementById("game-table");
-  var tbody = table.getElementsByTagName("tbody")[0];
-  tbody.innerHTML = ""; // Clear the table body before adding rows
+/**
+ * Gibt den angemeldeten Player zurück der über ID gesucht wird
+ *
+ * @param {*} data
+ * @param {*} id
+ * @returns
+ */
+function getPlayerByID(data, id) {
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      return data[i];
+    }
+  }
+  return null; // Wenn keine Übereinstimmung gefunden wurde
+}
+
+// Spieler laden für Double Spiel
+/**
+ * Karten werden angezeigt
+ */
+function showSpieler() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.addEventListener("load", ajaxShowSpieler);
+  xmlhttp.addEventListener("error", ajaxFehler);
+
+  xmlhttp.open("GET", "../php/spielerShow.php");
+  xmlhttp.send();
+}
+
+//karten anzeigen
+// die Ajaxanfrage wird in eine Json-Liste umgewandelt
+function ajaxShowSpieler(event) {
+  var myObj = JSON.parse(event.target.responseText);
+
+  for (var i = 0; i < myObj.length; i++) {
+    var id = myObj[i]["id"];
+    var spielname = myObj[i]["spielname"];
+    var email = myObj[i]["email"];
+    var passwort = myObj[i]["passwort"];
+    var level = myObj[i]["level"];
+
+    // Ein Objekt mit title und src erstellen
+    var item = {
+      id: id,
+      spielname: spielname,
+      email: email,
+      passwort: passwort,
+      level: level,
+    };
+
+    // Das erstellte Objekt der Liste hinzufügen
+
+    spielerList.push(item);
+  }
+
+  console.log(spielerList);
 }
