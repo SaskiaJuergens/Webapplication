b5e954a6df8a9de313dbca4d6f4a6dab8085d78e
Kris149x <131782916+Kris149x@users.noreply.github.com>
2023-08-13 00:53:46 +0200

    Try Edit and Delete Player

--- Website/php/editPlayer.php
+++ Website/php/editPlayer.php
@@ -5,87 +5,36 @@ include 'setupDB.php';
 
 global $conn;
 
-$spielname = $_POST['vorname'] .  ' ' . $_POST['nachname'];
-$nachname = $_POST['nachname'];
-$email = $_POST['email'];
-$passwort = $_POST['password'];
-$level = $_POST['level'];
-
-$show = '';
-
-if (isset($_POST["action"])) {
-  if ($_POST["action"] == "register") {
-    register();
-  } else if ($_POST["action"] == "login") {
-    login();
-  }
-}
-
-//testen ob, session check aufruf
-if (isset($_GET["action"]) && $_GET["action"] === "check") {
-    checkSession();
-}
-
-// REGISTER
-//ich habe erstmal die alte insert player methode genutzt die schon da war..
-// dort wird aber noch nicht abgefragt, ob es die mail schon gibt
-function register(){
-  global $conn;
- function isEmailRegistered($conn, $email) {
 
 
-    $sql = "SELECT COUNT(*) AS count FROM spieler WHERE email = '$email'";
-    $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        return $row['count'] > 0;
-    }
+// Überprüfen, ob das Formular abgeschickt wurde
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// Überprüfen, ob alle erforderlichen Felder ausgefüllt wurden
+if (!empty($_POST["spielname"]) && !empty($_POST["email"]) && !empty($_POST["level"]) && !empty($_POST["passwort"])) {
+// SQL-Abfrage zum Aktualisieren des Profils
+$sql = "UPDATE profiles SET spielname = ?, email = ?, level = ?, passwort = ? WHERE id = ?";
 
-    return false;
-}
 
-if (isEmailRegistered($conn, $email)) {
-    $show = '<h2>Der Spieler mit dieser E-Mail ist bereits registriert. Bitte verwende eine andere E-Mail.</h2>';
+// Vorbereiten der SQL-Abfrage
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssii", $_POST["spielname"], $_POST["email"], $_POST["level"], $_POST["passwort"]);
+
+// Ausführen der SQL-Abfrage
+if ($stmt->execute()) {
+    echo "Profil erfolgreich aktualisiert.";
 } else {
-    $sql = "INSERT INTO spieler (spielname, email, passwort, level) VALUES ('$spielname', '$email','$passwort','$level' )";
-    if ($conn->query($sql)) {
-        $show = '<h2>Der/Die Spieler*in ' . $spielname . ' wurde bearbeitet</h2>';
-    } else {
-        $show = '<h2>Ein Fehler ist aufgetreten. Bitte versuche es erneut.</h2>';
-    }
+    echo "Fehler beim Aktualisieren des Profils: " . $stmt->error;
 }
 
-echo $show;
+// Schließen des vorbereiteten Statements
+$stmt->close();
+} else {
+echo "Bitte füllen Sie alle erforderlichen Felder aus.";
 }
 
 
 
-// hier hole ich die daten des angemldeten spielers, oder finde raus, dass niemand angemeldet ist
-function checkSession() {
-      global $conn;
-    session_start();
-    $response = array();
-
-    if (isset($_SESSION["login"]) && $_SESSION["login"] === true) {
-        $response["isLoggedIn"] = true;
-        $response["spielerId"] = $_SESSION["id"];
-
-        $spieler = getSpieler($_SESSION["id"]);
-
-        if ($spieler) {
-            $response["email"] = $spieler["email"];
-            $response["passwort"] = $spieler["passwort"];
-            $response["spielname"] = $spieler["spielname"];
-            $response["level"] = $spieler["level"];
-        }
-    } else {
-        $response["isLoggedIn"] = false;
-    }
-
-    header("Content-Type: application/json");
-    echo json_encode($response);
-}
 
 // Verbindung zur Datenbank schließen
 $conn->close();
